1a81521e71c80ff852df475f1790cecd13cd3d4b
Dmitrii Ilichev <dsilichev@gmail.com>
2024-08-17 00:44:27 +0300

    Add jwt token

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const chalk = require("chalk");
 const path = require("path");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
 
 const {
   addNote,
@@ -10,7 +12,7 @@ const {
   removeNote,
 } = require("./notes-controller");
 
-const { addUser } = require("./users-controller");
+const { addUser, loginUser } = require("./users-controller");
 
 const port = 3000;
 
@@ -20,12 +22,33 @@ app.set("view engine", "ejs");
 app.set("views", "pages");
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.static(path.resolve(__dirname, "public")));
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
+
+app.get("/login", async (req, res) => {
+  res.render("login", {
+    title: "Express App",
+    error: undefined,
+  });
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const token = await loginUser(req.body.email, req.body.password);
+    res.cookie('token', token);
+    res.redirect("/");
+  } catch (e) {
+    res.render("login", {
+      title: "Express App",
+      error: e.message,
+    });
+  }
+});
 
 app.get("/register", async (req, res) => {
   res.render("register", {
--- users-controller.js
+++ users-controller.js
@@ -1,12 +1,30 @@
 const chalk = require("chalk");
 const User = require("./models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = 'test';
 
 async function addUser(email, password) {
   const passwordHash = await bcrypt.hash(password, 10);
   await User.create({ email, password: passwordHash });
 
   console.log(chalk.bgGreen.inverse("User was added"));
+}
+
+async function loginUser(email, password) {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error("User is not found");
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordCorrect) {
+    throw new Error("Password is not correct");
+  }
+
+  return jwt.sign({ email }, JWT_SECRET, { expiresIn: '30d'});
 }
 
 // async function getNotes() {
@@ -25,4 +43,4 @@ async function addUser(email, password) {
 //   console.log(chalk.bgYellow.inverse(`Note id:${noteData.id} was modified`));
 // }
 
-module.exports = { addUser };
+module.exports = { addUser, loginUser };
